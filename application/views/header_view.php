4da77df4bf574109ab44d9fbaca11c3585f90dae
WilliamChandra <blackzur@gmail.com>
2016-02-19 20:51:30 +0700

    FrontEnd
    -CSS sudah Mulai Nampak
    -Slider belum siap
    -Logo sudah masuk

--- application/views/header_view.php
+++
@@ -1,8 +0,0 @@
-
-<div class="navbar navbar-inverse navbar-fixed-top">
-  <div class="container">
-     <div class="left-navbar">
-		    <a href="#">
-                <img src="<?=base_url()?>img/logo.png"/></a>
-     </div>     
- </div>
--- application/views/parallax_view.php
+++ application/views/parallax_view.php
@@ -5,7 +5,7 @@
     <link href="<?php echo base_url(); ?>css/bootstrap.min.css" rel="stylesheet">
     <link href="<?php echo base_url(); ?>fonts/css/font-awesome.min.css" rel="stylesheet">
     <link href="<?php echo base_url(); ?>css/animate.min.css" rel="stylesheet">
-    
+
     <link href="<?=base_url()?>css/toppon.css" rel="stylesheet">
 
 </head>
@@ -15,27 +15,49 @@
 <!-- Fixed navbar -->
 <div class="navbar navbar-custom">
   <div class="container">
-     <div class="left-navbar">
+    <div class="left-navbar">
         <a href="#">
         <img src="<?=base_url()?>img/toppon.png"/></a>
-     </div>
+    </div>
      
-     <div class="right-navbar">
+    <div class="right-navbar">
           <ul>
             <li><a href="#">Blog</a></li>
             <li><a href="#">Login</a></li>
           </ul>
-     </div>
+    </div>
     
+    <div class="clearfix"> </div>
+
+    <div class="menu-navbar">
+    
+        <ul class="nav1">
+          <li><a href="#" data-toggle="modal" data-target="#myModal">Home</a></li>
+          <li><a href="#" data-toggle="modal" data-target="#myModal2">About Us</a></li>
+          <li><a href="#" data-toggle="modal" data-target="#myModal1">How To Use</a></li>
+          <li><a href="#" data-toggle="modal" data-target="#myModal3">Conversion Table</a></li>
+        </ul>
+            <!-- script-for-menu -->
+             <script>
+               $( "span.menu" ).click(function() {
+               $( "ul.nav1" ).slideToggle( 300, function() {
+               // Animation complete.
+                });
+               });
+            </script>
+            <!-- /script-for-menu -->
+    </div>
+
+
   </div>
 </div>
 <!--End Fixed Navbar-->
 
 <!--slider-->
 <section>
-<div id="about" name="about">
+<div class="slider-1" name="slider">
   <div class="container">
-    ABOUT US
+    SLIDER NOT READY
   </div>
 </div>
 
@@ -43,7 +65,7 @@
 <!--END slider-->
 
 <!--About Us-->
-<div id="about" name="about">
+<div class="about" name="about">
   <div class="container">
     ABOUT US
   </div>
@@ -52,7 +74,7 @@
 
 <!--Why Us?-->
 <section>
-<div id="about" name="about">
+<div class="whyus" name="whyus">
   <div class="container">
     WHY US?
   </div>
@@ -61,7 +83,7 @@
 <!--END Why Us?-->
 
 <!--How to Use-->
-<div id="about" name="about">
+<div class="howtouse" name="howtouse">
   <div class="container">
     HOW TO USE
   </div>
@@ -70,7 +92,7 @@
 
 <!--Conversion Table-->
 <section>
-<div id="about" name="about">
+<div class="conv-table" name="conv-table">
   <div class="container">
     CONVERSION TABLE
   </div>
