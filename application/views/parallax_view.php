ff043cbc32992870dcdade3d8c67052d3e0d9b01
WilliamChandra <blackzur@gmail.com>
2016-03-08 17:06:08 +0700

    Front End
    - Layer 6 ( How To Redeem)
    -Layer 7 Contact Us
    -Backto Top
    -Fonts

--- application/views/parallax_view.php
+++ application/views/parallax_view.php
@@ -2,6 +2,9 @@
 <head>
     <meta name="viewport" content="width=device-width, initial-scale=1.0"/>
     <title>Welcome to Toppon</title>
+
+    <!-- Reset CSS -->
+    <link href="<?php echo base_url(); ?>css/resetcss.css" rel="stylesheet">
     <!-- Bootstrap core CSS -->
     <link href="<?php echo base_url(); ?>css/bootstrap.min.css" rel="stylesheet">
     <link href="<?php echo base_url(); ?>fonts/css/font-awesome.min.css" rel="stylesheet">
@@ -15,6 +18,13 @@
 
     <!-- Bootstrap Core JavaScript -->
     <script src="<?php echo base_url(); ?>js/bootstrap.min.js"></script>
+    
+    <!-- CUSTOM FONTS -->
+    <link href="fonts/font-awesome/css/font-awesome.min.css" rel="stylesheet" type="text/css">
+    <link href="https://fonts.googleapis.com/css?family=Montserrat:400,700" rel="stylesheet" type="text/css">
+    <link href='https://fonts.googleapis.com/css?family=Kaushan+Script' rel='stylesheet' type='text/css'>
+    <link href='https://fonts.googleapis.com/css?family=Droid+Serif:400,700,400italic,700italic' rel='stylesheet' type='text/css'>
+    <link href='https://fonts.googleapis.com/css?family=Roboto+Slab:400,100,300,700' rel='stylesheet' type='text/css'>
 
 </head>
 
@@ -67,25 +77,23 @@
 
         <!-- Wrapper for Slides -->
         <div class="carousel-inner">
+
             <div class="item active">
                 <!-- Set the first background image using inline CSS below. -->
-                <div class="fill" style="background-image:url('../img/img1.jpg');"></div>
+                <div class="fill" style="background-image:url('../img/slider/battleborn_game.jpg');"></div>
                 <div class="carousel-caption">
-                    <h2>Caption 1</h2>
                 </div>
             </div>
             <div class="item">
                 <!-- Set the second background image using inline CSS below. -->
-                <div class="fill" style="background-image:url('../img/img2.png');"></div>
+                <div class="fill" style="background-image:url('../img/slider/EOS_3.jpg');"></div>
                 <div class="carousel-caption">
-                    <h2>Caption 2</h2>
                 </div>
             </div>
             <div class="item">
                 <!-- Set the third background image using inline CSS below. -->
-                <div class="fill" style="background-image:url('../img/img3.png');"></div>
+                <div class="fill" style="background-image:url('../img/slider/ro.jpg');"></div>
                 <div class="carousel-caption">
-                    <h2>Caption 3</h2>
                 </div>
             </div>
         </div>
@@ -220,24 +228,111 @@
 </section>
 <!--END Layer 5-->
 
-
-<div id="contact-us" name="contact-us">
+<!--How To Redeem-->
+<section>
+<div id="how-redeem">
     <div class="container">
-        <div class="content-text-w">
-            <div class="col-lg-6">
+       <div class="content-text">
+            <h1>HOW TO REDEEM</h1>
+        </div>
+        <div class="redeem-content">
+            <div class="titik-redeem">
+               <img src="<?php echo base_url();?>/img/howtoredeem/titik.png" class="img-responsive"/>
             </div>
-            <div class="col-lg-6">
-                <h1>Now get Voucher games</h1>
-                <h1>Very Easy</h1>
+
+            <div class="redeem-item" id="redeem-item-1">
+                <img src="<?php echo base_url();?>/img/howtoredeem/redeem1.png" class="img-responsive"/>
             </div>
-            <div class="whyus-content">
-                <p>easy and secure for payment voucher games is our priority!</p>
+            <div class="redeem-item" id="redeem-item-2">
+                <img src="<?php echo base_url();?>/img/howtoredeem/redeem2.png" class="img-responsive"/>
             </div>
-        </div>    
+            <div class="redeem-item" id="redeem-item-3">
+                <img src="<?php echo base_url();?>/img/howtoredeem/redeem3.png" class="img-responsive"/>
+            </div>
+            <div class="redeem-item" id="redeem-item-4">
+                <img src="<?php echo base_url();?>/img/howtoredeem/redeem4.png" class="img-responsive"/>
+            </div>
+            <div class="redeem-item" id="redeem-item-5">
+                <img src="<?php echo base_url();?>/img/howtoredeem/redeem5.png" class="img-responsive"/>
+            </div>
+        </div>
     </div>
 </div>
+</section>
+<!--End How To Redeem-->
 
 
+<!--Contact US-->
+<div id="contact-us" name="contact-us">
+    <div class="content-text">
+        <h1>CONTACT US</h1>
+    </div>
+
+    <div class="row">
+    <div class="col-lg-6">
+        <div class="contact-form-container">
+        <!--ContactForm-->
+        <h3>Have any question for us?</h3>
+            <form id="contactForm" name="sentMessage" class="form" novalidate="">
+            <div class="row">
+                <div id="success_ajax" class="text-center"> </div>
+            </div>
+            <div class="row">
+                <div id="contactName-group" class="col-xs-12 col-sm-6 col-md-6 form-group">
+                    <input type="text" id="contactName" class="form-control" placeholder="Name" required="required">
+                    <p class="help-block text-danger"></p>
+                </div>
+                <div id="contactEmail-group" class="col-xs-12 col-sm-6 col-md-6 form-group">
+                    <input type="email" id="contactEmail" class="form-control" placeholder="Email" required="required">
+                    <p class="help-block text-danger"></p>
+                </div>
+                <div id="contactSubject-group" class="col-xs-12 col-md-12 form-group">
+                    <input type="text" id="contactSubject" class="form-control" placeholder="Subject" required="required">
+                    <p class="help-block text-danger"></p>
+                </div>
+                <div id="contactMessage-group" class="col-xs-12 col-md-12 form-group">
+                    <textarea name="contactMessage" id="contactMessage" class="form-control" rows="5" placeholder="Message" required=""></textarea>
+                    <p class="help-block text-danger"></p>
+                </div>
+            </div>
+
+            <div id="success"></div>
+                <button class="btn btn-lg btn-default" type="submit">Kirim Pesan</button>
+        </form>
+        <!-- form -->
+        </div>
+    </div>
+        <div class="col-lg-6">
+            <div class="contact-detailed">
+                <div class="telepon">
+                    <div class="row">
+                        <div class="col-lg-3">
+                            <img src="<?php echo base_url();?>/img/phone.png" class="img-responsive">
+                        </div>
+                        <div class="col-lg-9 no-tlp">
+                            <p>+62-812-9025-5465</p>
+                        </div>
+                    </div>
+                </div>
+                <div class="mail">
+                    <div class="row">
+                        <div class="col-lg-3">
+                            <img src="<?php echo base_url();?>/img/mail.png" class="img-responsive">
+                        </div>
+                        <div class="col-lg-9 dest-mail">
+                            <p>cs@toppon.co.id</p>
+                        </div>
+                    </div>
+                </div>
+                <div class="alamat">
+                    <p>Jl. Kebon Jeruk No.27 Kemanggisan, Jakarta Barat</p>
+                </div>
+            </div>
+        </div>
+    </div>    
+</div>
+
+<!-- Footer
 <div id="footer" name="footer">
     <div class="container">
         <div class="content-text-w">
@@ -253,6 +348,40 @@
         </div>    
     </div>
 </div>
+
+End footer -->
+
+<!-- BUTTON BACK TO TOP HOME -->
+<div style = "visibility:visible;" id="back_to_top">
+    <a href="#home"> 
+        <img class="img img-circle" src="<?php echo base_url();?>/img/back-to-top.png" height="50px" width="50px" alt="back-to-top">
+    </a>
+</div>
+<!-- END BUTTON BACK TO TOP HOME -->
+    
+
+ <!-- SCRIPT BACK TO TOP -->
+    <script src="<?php echo base_url();?>js/jquery.easing.js"></script>
+
+    <script>
+        $(document).ready(function() {
+            var offset = 250;
+            var duration = 600;
+            $(window).scroll(function() {
+                if ($(this).scrollTop() > offset) {
+                    $("#back_to_top").fadeIn(duration);
+                }
+                else {
+                    $("#back_to_top").fadeOut(duration);
+                }
+            });
+            $("#back_to_top").click(function(event) {
+                event.preventDefault();
+                $("html, body").animate({scrollTop: 0}, 1000, 'easeInOutExpo');
+                return false;
+            })
+        });
+    </script>
     <!-- Script to Activate the Carousel -->
     <script>
         // Script for menu
