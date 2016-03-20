1550e58d3237e1db70bfc510cf813e472cabbddf
valend69 <valend.valend07@gmail.com>
2016-03-21 03:57:50 +0700

    Backend Home
    revisi penamaan

--- application/views/admin/home_list_view.php
+++ application/views/admin/home_list_view.php
@@ -24,7 +24,7 @@
     <div class="col-md-12 col-sm-12 col-xs-12">
         <div class="x_panel">
             <div class="x_title">
-                <h2>Home List<small>List of Home Details</small></h2>
+                <h2>Home List<small>List of Homepage Contents Details</small></h2>
                 <div class="clearfix"></div>
             </div>
             <div class="x_content">
