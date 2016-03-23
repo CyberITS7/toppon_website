65054880f9d4410522f81a03ff3e2c2e945808a2
Aslela <vickysiswanto@gmail.com>
2016-03-23 15:24:09 +0700

    Nominal benerin datatable bug
    Nominal benerin datatable bug

--- application/views/admin/nominal_list_view.php
+++ application/views/admin/nominal_list_view.php
@@ -161,7 +161,7 @@
             $('#nominal-form')[0].reset();
         });
 
-        $('.btn-edit').click(function(){
+        $(document).on( "click", ".btn-edit", function() {
             $('.label-danger').html('');
             // Set Title modal
             $('.modal-title').html('Edit Nominal');
@@ -211,7 +211,7 @@
             }
         });
 
-        $('.btn-delete').click(function(){
+        $(document).on( "click", ".btn-delete", function() {
             var row = $(this).closest("tr");
             var col_title =  row.find(".td-nominal-name").text();
             var col_id =  row.find("input.item-id").val();
